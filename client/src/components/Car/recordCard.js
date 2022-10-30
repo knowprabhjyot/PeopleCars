04895a5bc05ed612da00f8bd1df67c5e09da2c6e
Prabhjyot Gambhir <knowprabhjyot@gmail.com>
2022-10-30 11:49:49 -0700

    fixed bug

--- client/src/components/Car/recordCard.js
+++ client/src/components/Car/recordCard.js
@@ -1,63 +1,63 @@
-import { Box, IconButton, Typography } from "@mui/material";
-import RemoveCar from "./removeCar";
-import Divider from "@mui/material/Divider";
-import { Link } from "react-router-dom";
-import UpdateCar from "./updateCar";
-import { useState } from "react";
-import EditButton from "@mui/icons-material/Edit";
+  import { Box, IconButton, Typography } from "@mui/material";
+  import RemoveCar from "./removeCar";
+  import Divider from "@mui/material/Divider";
+  import { Link } from "react-router-dom";
+  import UpdateCar from "./updateCar";
+  import { useState } from "react";
+  import EditButton from "@mui/icons-material/Edit";
 
-const RecordCard = ({ data }) => {
-  const [editMode, setEditMode] = useState(false);
-  const handleButtonClick = () => setEditMode(!editMode);
+  const RecordCard = ({ data }) => {
+    const [editMode, setEditMode] = useState(false);
+    const handleButtonClick = () => setEditMode(!editMode);
 
-  return (
-    <>
-      <Box
-        display="flex"
-        flexDirection="column"
-        border="1px solid gray"
-        width="100%"
-        padding="20px"
-      >
-        <h4>
-          {data.firstName} {data.lastName}
-        </h4>
-        <Box display="flex" flexDirection="column" gap="20px">
-          {data.cars.map((val, index) => {
-            return (
-              <Box key={index}>
-                <Box backgroundColor="#d1d1d1">
-                  {" "}
-                  <Typography variant="h6">
-                    {val.year} {val.make} {"--> $"} {val.price}
-                  </Typography>
+    return (
+      <>
+        <Box
+          display="flex"
+          flexDirection="column"
+          border="1px solid gray"
+          width="100%"
+          padding="20px"
+        >
+          <h4>
+            {data.firstName} {data.lastName}
+          </h4>
+          <Box display="flex" flexDirection="column" gap="20px">
+            {data.cars?.map((val, index) => {
+              return (
+                <Box key={index}>
+                  <Box backgroundColor="#d1d1d1">
+                    {" "}
+                    <Typography variant="h6">
+                      {val.year} {val.make} {"--> $"} {val.price}
+                    </Typography>
+                  </Box>
+                  <Box display="flex" justifyContent="flex-end">
+                    {editMode ? (
+                      <UpdateCar data={val} onButtonClick={handleButtonClick} />
+                    ) : null}
+                    <IconButton
+                      onClick={handleButtonClick}
+                      color="secondary"
+                      aria-label="upload picture"
+                      component="label"
+                    >
+                      <EditButton />
+                    </IconButton>
+                    <RemoveCar data={val} />
+                  </Box>
                 </Box>
-                <Box display="flex" justifyContent="flex-end">
-                  {editMode ? (
-                    <UpdateCar data={val} onButtonClick={handleButtonClick} />
-                  ) : null}
-                  <IconButton
-                    onClick={handleButtonClick}
-                    color="secondary"
-                    aria-label="upload picture"
-                    component="label"
-                  >
-                    <EditButton />
-                  </IconButton>
-                  <RemoveCar data={val} />
-                </Box>
-              </Box>
-            );
-          })}
-        </Box>
+              );
+            })}
+          </Box>
 
-        <Box padding="20px">
-          <Divider />
+          <Box padding="20px">
+            <Divider />
+          </Box>
+          <Link to="about">Learn More</Link>
         </Box>
-        <Link to="about">Learn More</Link>
-      </Box>
-    </>
-  );
-};
+      </>
+    );
+  };
 
-export default RecordCard;
+  export default RecordCard;
